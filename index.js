573d2949d48c478790f80fd031b16c474e9a1566
Montu-Gohain <leomontugohain@gmail.com>
2023-02-10 16:00:34 +0530

    Removed package installed in ..sec text via using a package called clear-any-console by Awais Ahmed

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const clearConsole = require("clear-any-console");
+
+clearConsole();
+console.log(`
+Montu Gohain
+
+A newbie javascript developer learing NodeJS and React to build some cool stuff with it.Currently learning to how to create cli apps using his nodejs knowledge.
+
+ `);
